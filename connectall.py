cb3fed8c286fc6d1a083f18a3ab361bb2fd079ca
Jason Wessel <jason.wessel@windriver.com>
2022-01-11 15:39:36 +0000

    Provide a python connectall script.
    The ruby version is much slower.
    
    % time python3 connectall.py
    
    real    0m0.488s
    user    0m0.383s
    sys     0m0.102s
    
    % time ruby connectall.rb
    
    real    0m1.071s
    user    0m0.843s
    sys     0m0.227s
    
    This also allows ruby to be removed from the OS image.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

---
+++ connectall.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import subprocess
+
+ports = subprocess.check_output(["aconnect", "-i", "-l"], text=True)
+port_list = []
+client = "0"
+for line in str(ports).splitlines():
+    if line.startswith("client "):
+        client = line[7:].split(":",2)[0]
+        if client == "0" or "Through" in line:
+            client = "0"
+    else:
+        if client == "0" or line.startswith('\t'):
+            continue
+        port = line.split()[0]
+        port_list.append(client+":"+port)
+for source in port_list:
+    for target in port_list:
+        if source != target:
+            #print("aconnect %s %s" % (source, target))
+            subprocess.call("aconnect %s %s" % (source, target), shell=True)
