3f1aa935f217d0f9e9c8c749a8f1ac97f1125299
Jason Wessel <jason.wessel@windriver.com>
2022-01-11 10:34:29 -0600

    Fix midi loop issue and dangling devices with rtpmidid
    Everytime the playback device selection was used a new RtMidi endpoint
    was created.  Using the connect all after this was causing completely
    erattic behavior such as loops and lag.  The old endpoints must be
    deleted before creating new endpoints.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- lib/midiports.py
+++ lib/midiports.py
@@ -6,6 +6,8 @@ class MidiPorts:
         self.usersettings = usersettings
         self.pending_queue = []
         self.last_activity = 0
+        self.inport = None
+        self.playport = None
 
         # checking if the input port was previously set by the user
         port = self.usersettings.get_setting_value("input_port")
@@ -67,6 +69,8 @@ class MidiPorts:
             for target in port_list:
                 if source != target:
                     subprocess.call("sudo aconnect %s %s" % (source, target), shell=True)
+        # Reconnect the input and playports on a connectall
+        self.reconnect_ports()
 
     def add_instance(self, menu):
         self.menu = menu
@@ -74,9 +78,15 @@ class MidiPorts:
     def change_port(self, port, portname):
         try:
             if port == "inport":
+                if self.inport != None:
+                    self.inport.close()
+                    self.inport = None
                 self.inport = mido.open_input(portname)
                 self.usersettings.change_setting_value("input_port", portname)
             elif port == "playport":
+                if self.playport != None:
+                    self.playport.close()
+                    self.playport = None
                 self.playport = mido.open_output(portname)
                 self.usersettings.change_setting_value("play_port", portname)
             self.menu.render_message("Changing " + port + " to:", portname, 1500)
@@ -85,11 +95,17 @@ class MidiPorts:
 
     def reconnect_ports(self):
         try:
+            if self.inport != None:
+                self.inport.close()
+                self.inport = None
             port = self.usersettings.get_setting_value("input_port")
             self.inport = mido.open_input(port)
         except:
             print("Can't reconnect input port: " + port)
         try:
+            if self.playport != None:
+                self.playport.close()
+                self.playport = None
             port = self.usersettings.get_setting_value("play_port")
             self.playport = mido.open_output(port)
         except:
