c861ba3155e55e13af7f6ed035512250bbb9da61
onlaj <onlyoneaw@gmail.com>
2023-08-11 14:12:10 +0200

    Add disable_ap.sh

---
+++ lib/disable_ap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sleep 3
+
+# disable the AP
+sudo cp config/hostapd.disabled /etc/default/hostapd
+sudo cp config/dhcpcd.conf.disabled /etc/dhcpcd.conf
+sudo cp config/dnsmasq.conf.disabled /etc/dnsmasq.conf
+
+# load wlan configuration
+sudo cp wpa_disable_ap.conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+sleep 5
+sudo wpa_cli -i wlan0 reconfigure
+sudo wpa_cli -i p2p-dev-wlan0
+sleep 5
+sudo ifconfig wlan0 down
+sleep 10
+sudo ifconfig wlan0 up
