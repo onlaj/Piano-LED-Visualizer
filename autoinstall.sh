8cc20b5769956439c26fb7570740027723aab30d
onlaj <onlyoneaw@gmail.com>
2023-08-31 13:04:20 +0200

    Create an autoinstall script

---
+++ autoinstall.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# Function to display error message and exit
+display_error() {
+  echo "Error: $1" >&2
+  exit 1
+}
+
+# Function to execute a command and check for errors
+execute_command() {
+  echo "Executing: $1"
+  eval "$1"
+  local exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    display_error "Command failed with exit code $exit_code."
+  fi
+}
+
+# Function to update the OS
+update_os() {
+  execute_command "sudo apt-get update"
+  execute_command "sudo apt-get upgrade -y"
+}
+
+# Function to create and configure the autoconnect script
+configure_autoconnect_script() {
+  # Create connectall.py file
+  execute_command "sudo nano /usr/local/bin/connectall.py"
+  cat <<EOF | sudo tee /usr/local/bin/connectall.py > /dev/null
+#!/usr/bin/python3
+import subprocess
+
+ports = subprocess.check_output(["aconnect", "-i", "-l"], text=True)
+port_list = []
+client = "0"
+for line in str(ports).splitlines():
+    if line.startswith("client "):
+        client = line[7:].split(":",2)[0]
+        if client == "0" or "Through" in line:
+            client = "0"
+    else:
+        if client == "0" or line.startswith('\t'):
+            continue
+        port = line.split()[0]
+        port_list.append(client+":"+port)
+for source in port_list:
+    for target in port_list:
+        if source != target:
+            subprocess.call("aconnect %s %s" % (source, target), shell=True)
+EOF
+
+  # Change permissions
+  execute_command "sudo chmod +x /usr/local/bin/connectall.py"
+
+  # Create udev rules file
+  execute_command "sudo nano /etc/udev/rules.d/33-midiusb.rules"
+  echo 'ACTION=="add|remove", SUBSYSTEM=="usb", DRIVER=="usb", RUN+="/usr/local/bin/connectall.py"' | sudo tee -a /etc/udev/rules.d/33-midiusb.rules > /dev/null
+
+  # Reload services
+  execute_command "sudo udevadm control --reload"
+  execute_command "sudo service udev restart"
+
+  # Create midi.service file
+  execute_command "sudo nano /lib/systemd/system/midi.service"
+  cat <<EOF | sudo tee /lib/systemd/system/midi.service > /dev/null
+[Unit]
+Description=Initial USB MIDI connect
+
+[Service]
+ExecStart=/usr/local/bin/connectall.py
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  # Reload daemon and enable service
+  execute_command "sudo systemctl daemon-reload"
+  execute_command "sudo systemctl enable midi.service"
+  execute_command "sudo systemctl start midi.service"
+}
+
+# Function to enable SPI interface
+enable_spi_interface() {
+  # Edit config.txt file to enable SPI interface
+  execute_command "sudo sed -i '$ a\dtparam=spi=on' /boot/config.txt"
+}
+
+# Function to install required packages
+install_packages() {
+  execute_command "sudo apt-get install -y ruby git python3-pip autotools-dev libtool autoconf libasound2-dev libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev python-dev libatlas-base-dev libopenjp2-7 libtiff5 libjack0 libjack-dev libasound2-dev fonts-freefont-ttf gcc make build-essential python-dev git scons swig libavahi-client3 abcmidi"
+}
+
+# Function to disable audio output
+disable_audio_output() {
+  # Edit snd-blacklist.conf file
+  execute_command "sudo sed -i '$ a\blacklist snd_bcm2835' /etc/modprobe.d/snd-blacklist.conf"
+
+  # Edit config.txt file
+  execute_command "sudo sed -i 's/dtparam=audio=on/#dtparam=audio=on/' /boot/config.txt"
+}
+
+# Function to install RTP-midi server
+install_rtpmidi_server() {
+  execute_command "cd /home/"
+  execute_command "sudo wget https://github.com/davidmoreno/rtpmidid/releases/download/v20.07/rtpmidid_20.07_armhf.deb"
+  execute_command "sudo dpkg -i rtpmidid_20.07_armhf.deb"
+}
+
+# Function to create Hot-Spot
+create_hotspot() {
+  execute_command "sudo systemctl stop dnsmasq && sudo systemctl stop hostapd"
+  echo 'interface wlan0 static ip_address=192.168.4.1/24' | sudo tee --append /etc/dhcpcd.conf
+  sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+  sudo systemctl daemon-reload
+  sudo systemctl restart dhcpcd
+  echo 'interface=wlan0 dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h' | sudo tee --append /etc/dnsmasq.conf
+  echo 'interface=wlan0 driver=nl80211 ssid=PianoLEDVisualizer hw_mode=g channel=7 wmm_enabled=0 macaddr_acl=0 auth_algs=1 ignore_broadcast_ssid=0 wpa=2 wpa_passphrase=visualizer wpa_key_mgmt=WPA-PSK wpa_pairwise=TKIP rsn_pairwise=CCMP' | sudo tee --append /etc/hostapd/hostapd.conf
+  echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' | sudo tee --append /etc/default/hostapd
+  execute_command "sudo systemctl start hostapd && sudo systemctl start dnsmasq"
+}
+
+# Function to install Piano-LED-Visualizer
+install_piano_led_visualizer() {
+  execute_command "cd /home/"
+  execute_command "sudo git clone https://github.com/onlaj/Piano-LED-Visualizer"
+  execute_command "cd Piano-LED-Visualizer"
+  execute_command "sudo pip3 install -r requirements.txt"
+  execute_command "sudo raspi-config nonint do_boot_behaviour B2"
+  execute_command "sudo nano /lib/systemd/system/visualizer.service"
+  cat <<EOF | sudo tee /lib/systemd/system/visualizer.service > /dev/null
+[Unit]
+Description=Piano LED Visualizer
+After=network-online.target
+Wants=network-online.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=sudo python3 /home/Piano-LED-Visualizer/visualizer.py
+Type=simple
+User=pi
+Group=pi
+EOF
+  execute_command "sudo systemctl daemon-reload"
+  execute_command "sudo systemctl enable visualizer.service"
+  execute_command "sudo systemctl start visualizer.service"
+
+  # Reboot Raspberry Pi
+  execute_command "sudo reboot"
+}
+
+# Main script execution
+update_os
+configure_autoconnect_script
+enable_spi_interface
+install_packages
+disable_audio_output
+install_rtpmidi_server
+create_hotspot
+install_piano_led_visualizer
