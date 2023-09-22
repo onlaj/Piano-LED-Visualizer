6a873848be6a5424c2e0852a06796feb7631c582
onlaj <onlyoneaw@gmail.com>
2023-09-22 20:37:45 +0200

    Shorten sleep time in scripts

--- enable_ap.sh
+++ enable_ap.sh
@@ -13,11 +13,11 @@ sudo cp config/wpa_enable_ap.conf /etc/wpa_supplicant/wpa_supplicant.conf
 sleep 1
 sudo wpa_cli -i wlan0 reconfigure
 sudo wpa_cli -i p2p-dev-wlan0 reconfigure
-sleep 5
+sleep 2
 sudo ifconfig wlan0 down
-sleep 5
+sleep 3
 sudo ifconfig wlan0 up
-sleep 8
+sleep 2
 
 # Check if hostapd is masked, and unmask it if needed
 if [[ $(sudo systemctl is-enabled hostapd) == "masked" ]]; then
