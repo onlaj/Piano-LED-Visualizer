d16eca69be7def064e5c469fe0eaf3c6625b78e6
onlaj <onlyoneaw@gmail.com>
2024-09-27 23:53:19 +0200

    Refactor and simplify hotspot management methods

--- lib/platform.py
+++ lib/platform.py
@@ -79,153 +79,101 @@ class PlatformRasp:
             logger.warning(f"Error checking {package_name} package status")
             return False
 
-    def manage_hotspot(self, hotspot, usersettings, midiports, first_run=False):
-        if not hotspot.is_hostapd_installed:
+    def create_hotspot_profile(self):
+        # Check if the 'Hotspot' profile already exists
+        check_profile = subprocess.run(['sudo', 'nmcli', 'connection', 'show', 'Hotspot'],
+                                       capture_output=True, text=True)
+
+        if 'Hotspot' in check_profile.stdout:
+            print("Hotspot profile already exists. Skipping creation.")
             return
 
-        # Visualizer is starting, check if hotspot is active and run enable_ap.sh
-        if first_run:
-            subprocess.call(['sudo', 'chmod', '+x', './disable_ap.sh'])
-            subprocess.call(['sudo', 'chmod', '+x', './enable_ap.sh'])
-            subprocess.call(['sudo', 'chmod', '+w', './config/wpa_disable_ap.conf'])
+        # If we reach here, the profile doesn't exist, so we create it
+        print("Creating new Hotspot profile...")
 
-            self.create_hotspot_config()
+        try:
+            subprocess.run([
+                'sudo', 'nmcli', 'connection', 'add', 'type', 'wifi', 'ifname', 'wlan0',
+                'con-name', 'Hotspot', 'autoconnect', 'no', 'ssid', 'PianoLEDVisualizer'
+            ], check=True)
+
+            subprocess.run([
+                'sudo', 'nmcli', 'connection', 'modify', 'Hotspot',
+                '802-11-wireless.mode', 'ap', '802-11-wireless.band', 'bg',
+                'ipv4.method', 'shared'
+            ], check=True)
+
+            subprocess.run([
+                'sudo', 'nmcli', 'connection', 'modify', 'Hotspot',
+                'wifi-sec.key-mgmt', 'wpa-psk'
+            ], check=True)
+
+            subprocess.run([
+                'sudo', 'nmcli', 'connection', 'modify', 'Hotspot',
+                'wifi-sec.psk', 'visualizer'
+            ], check=True)
+
+            print("Hotspot profile created successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred while creating the Hotspot profile: {e}")
+
+    def enable_hotspot(self):
+        subprocess.run(['sudo', 'nmcli', 'connection', 'up', 'Hotspot'])
+
+    def disable_hotspot(self):
+        subprocess.run(['sudo', 'nmcli', 'connection', 'down', 'Hotspot'])
+
+    def get_current_connections(self):
+        result = subprocess.run(['nmcli', '-t', '-f', 'NAME,TYPE,DEVICE', 'connection', 'show', '--active'],
+                                capture_output=True, text=True)
+        connections = result.stdout.strip().split('\n')
+        for conn in connections:
+            name, type, device = conn.split(':')
+            if type == 'wifi' and device == 'wlan0':
+                return True, name, ''
+        return False, "Not connected to any Wi-Fi network.", ""
+
+
+
+    def manage_hotspot(self, hotspot, usersettings, midiports, first_run=False):
+        if first_run:
+            self.create_hotspot_profile()
             if int(usersettings.get("is_hotspot_active")):
-                self.disconnect_from_wifi(hotspot, usersettings)
+                self.enable_hotspot()
                 return
             elif int(usersettings.get_setting_value("reinitialize_network_on_boot")) == 1:
-                try:
-                    logger.info("Running disable_ap.sh")
-                    subprocess.Popen(['sudo', './disable_ap.sh'], stdout=subprocess.DEVNULL,
-                                     stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
-                except Exception as error:
-                    # handle the exception
-                    logger.info("An exception occurred while shutting down a hotspot:", error)
+                self.disable_hotspot()
 
-        # Calculate time passed without Wi-fi
         current_time = time.time()
         if not hotspot.last_wifi_check_time:
             hotspot.last_wifi_check_time = current_time
 
-        time_without_wifi = current_time - hotspot.last_wifi_check_time
-
-        # run script every 60 seconds
-        if (time.time() - hotspot.hotspot_script_time) > 60 and time.time() - midiports.last_activity > 60:
+        if (current_time - hotspot.hotspot_script_time) > 60 and (current_time - midiports.last_activity) > 60:
             hotspot.hotspot_script_time = current_time
             if int(usersettings.get("is_hotspot_active")):
                 return
 
-            # check if wi-fi is connected
-            wifi_success, wifi_ssid, address = self.get_current_connections()
+            wifi_success, wifi_ssid, _ = self.get_current_connections()
 
             if not wifi_success:
-                # Update the time without Wi-Fi
-                hotspot.time_without_wifi += time_without_wifi
-                logger.info("Time without Wi-Fi: ", hotspot.time_without_wifi)
-
-                # If hotspot.time_without_wifi is greater than 240 seconds, start hotspot
+                hotspot.time_without_wifi += (current_time - hotspot.last_wifi_check_time)
                 if hotspot.time_without_wifi > 240:
                     usersettings.change_setting_value("is_hotspot_active", 1)
-                    time.sleep(2)
-                    self.disconnect_from_wifi(hotspot, usersettings)
+                    self.enable_hotspot()
             else:
-                # Reset the time without Wi-Fi since there is a connection now
                 hotspot.time_without_wifi = 0
 
-    def create_hotspot_config(self):
-        hotspot_config_content = """
-interface=wlan0
-driver=nl80211
-ssid=PianoLEDVisualizer
-hw_mode=g
-channel=7
-wmm_enabled=0
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=visualizer
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
-rsn_pairwise=CCMP
-"""
+        hotspot.last_wifi_check_time = current_time
 
-        filepath = '/etc/hostapd/hostapd.conf'
-
-        try:
-            # Check if the file doesn't exist or is empty
-            if not (os.path.exists(filepath) and os.path.getsize(filepath) > 0):
-                with open(filepath, 'w') as file:
-                    file.write(hotspot_config_content)
-                logger.info("Hotspot configuration added successfully.")
-        except Exception as e:
-            logger.warning(f"Error: {e}")
-
-    def get_current_connections(self):
-        try:
-            with open(os.devnull, 'w') as null_file:
-                output = subprocess.check_output(['iwconfig'], text=True, stderr=null_file)
-
-            for line in output.split('\n'):
-                if "ESSID:" in line:
-                    ssid = line.split("ESSID:")[-1].strip().strip('"')
-                    if ssid != "off/any":
-                        access_point_line = [line for line in output.split('\n') if "Access Point:" in line]
-                        if access_point_line:
-                            access_point = access_point_line[0].split("Access Point:")[1].strip()
-                            return True, ssid, access_point
-                        else:
-                            return False, "Not connected to any Wi-Fi network.", ""
-                    else:
-                        return False, "Not connected to any Wi-Fi network.", ""
-
-            return False, "No Wi-Fi interface found.", ""
-        except subprocess.CalledProcessError:
-            return False, "Error occurred while getting Wi-Fi information.", ""
-
-    def connect_to_wifi(self, ssid, password, hotspot, usersettings):
-        hotspot.hotspot_script_time = time.time()
-        logger.info("Method:connecting to wifi")
-        success, wifi_ssid, address = self.get_current_connections()
-
-        if success:
-            if wifi_ssid == ssid:
-                logger.info("Already connected to Wi-Fi:", ssid)
-                return True
-
-        wpa_conf = """country=GB
-        ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-        update_config=1
-        network={
-            scan_ssid=1
-            ssid="%s"
-            %s
-        }"""
-
-        pwd = 'psk="' + password + '"'
-        if password == "":
-            pwd = "key_mgmt=NONE"  # If open AP
-
-        with open('config/wpa_disable_ap.conf', 'w') as f:
-            f.write(wpa_conf % (ssid, pwd))
-        logger.info("Running shell script disable_ap")
-        try:
-            subprocess.Popen(['sudo', './disable_ap.sh'], stdout=subprocess.DEVNULL,
-                             stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
-        except Exception as error:
-            # handle the exception
-            logger.warning("An exception occurred while shutting down a hotspot:", error)
-        usersettings.change_setting_value("is_hotspot_active", 0)
+    def connect_to_wifi(ssid, password):
+        subprocess.run([
+            'sudo', 'nmcli', 'device', 'wifi', 'connect', ssid,
+            'password', password
+        ])
 
     def disconnect_from_wifi(self, hotspot, usersettings):
         hotspot.hotspot_script_time = time.time()
-        logger.info("Running script enable_ap")
-        try:
-            subprocess.Popen(['sudo', './enable_ap.sh'], stdout=subprocess.DEVNULL,
-                             stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
-        except Exception as error:
-            # handle the exception
-            logger.warning("An exception occurred while creating a hotspot:", error)
+        self.enable_hotspot()
         usersettings.change_setting_value("is_hotspot_active", 1)
 
     def get_wifi_networks(self):
