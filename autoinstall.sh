e633231562d70a72c3aeca1d4a6ba6a324151086
onlaj <onlyoneaw@gmail.com>
2024-12-24 15:54:12 +0100

    Revert rtpmidi server to rtpmidid Update rtpmidid

--- autoinstall.sh
+++ autoinstall.sh
@@ -114,7 +114,7 @@ enable_spi_interface() {
 
 # Function to install required packages
 install_packages() {
-  execute_command "sudo apt-get install -y ruby git python3-pip autotools-dev libtool autoconf libopenblas-dev libasound2-dev libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev python3 libatlas-base-dev libopenjp2-7 libtiff6 libjack0 libjack-dev libasound2-dev fonts-freefont-ttf gcc make build-essential git scons swig libavahi-client3 abcmidi" "check_internet"
+  execute_command "sudo apt-get install -y ruby git python3-pip autotools-dev libtool autoconf libasound2 libavahi-client3 libavahi-common3 libc6 libfmt9 libgcc-s1 libstdc++6 python3 libopenblas-dev libavahi-client-dev libasound2-dev libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev libatlas-base-dev libopenjp2-7 libtiff6 libjack0 libjack-dev fonts-freefont-ttf gcc make build-essential scons swig abcmidi" "check_internet"
 }
 
 # Function to disable audio output
@@ -126,13 +126,10 @@ disable_audio_output() {
 # Function to install RTP-midi server
 install_rtpmidi_server() {
   execute_command "cd /home/"
-  execute_command "sudo wget https://ftp.debian.org/debian/pool/main/f/fmtlib/libfmt7_7.1.3+ds1-5_armhf.deb"
-  execute_command "sudo dpkg -i libfmt7_7.1.3+ds1-5_armhf.deb"
-  execute_command "rm libfmt7_7.1.3+ds1-5_armhf.deb"
-  execute_command "sudo wget https://github.com/davidmoreno/rtpmidid/releases/download/v23.10/rtpmidid_23.10_armhf.deb" "check_internet"
-  execute_command "sudo dpkg -i rtpmidid_23.10_armhf.deb"
+  execute_command "sudo wget https://github.com/davidmoreno/rtpmidid/releases/download/v24.12/rtpmidid_24.12.2_armhf.deb" "check_internet"
+  execute_command "sudo dpkg -i rtpmidid_24.12.2_armhf.deb"
   execute_command "sudo apt -f install"
-  execute_command "rm rtpmidid_20.07_armhf.deb"
+  execute_command "rm rtpmidid_24.12.2_armhf.deb"
 }
 
 
