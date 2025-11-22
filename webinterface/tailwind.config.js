be418e8248efed345b26bcb3c3cf70ba0b6baa94
onlaj <onlyoneaw@gmail.com>
2025-11-22 14:54:55 +0100

    Improve ledsettings tab layout

--- webinterface/tailwind.config.js
+++ webinterface/tailwind.config.js
@@ -1,18 +1,16 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: {
-    content: ['./templates/*.html', './static/*.{html,js}'],
-      safelist: [
-        'animate-spin',
-        'top-1/2',
-        'top-1/4',
-        'right-2',
-        'opacity-20',
-        'opacity-30',
-        '-translate-y-1/2'
-      ]
-  },
+  content: ['./templates/*.html', './static/*.{html,js}'],
+  safelist: [
+    'animate-spin',
+    'top-1/2',
+    'top-1/4',
+    'right-2',
+    'opacity-20',
+    'opacity-30',
+    '-translate-y-1/2'
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
       colors: {
